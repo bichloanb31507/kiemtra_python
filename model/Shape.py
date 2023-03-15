edea2ea08c84f887a190c98adb021fe4d330e0de
Bich Loan <bichloanb31507@gmail.com>
2023-03-15 07:40:15 +0700

    update

--- model/Shape.py
+++ model/Shape.py
@@ -2,9 +2,9 @@ from abc import ABC, abstractmethod
 
 class Shape(ABC):
     
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(x : int, y: int):
+        x = x
+        y = y
         
     @abstractmethod
     def chuVi():
